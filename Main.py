bafa585279cb94dea18486d3c9c465d3414caea8
Jmanderson1997 <34497604+Jmanderson1997@users.noreply.github.com>
2018-02-06 15:06:23 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,22 @@
+from Encoder import encoder
+from Decoder import decoder
+import filecmp 
+
+orig= open("Example.txt",'r')
+trans= open("TranslatedFile.txt",'r+')
+new= open("RevertedFile.txt",'r+') 
+
+trans.truncate()
+new.truncate()
+
+encoder('Example.txt', "TranslatedFile.txt")
+decoder("TranslatedFile.txt", "RevertedFile.txt")
+
+#print("Old text")
+#print(orig.read())
+#print("DNA Sequence")
+#print(trans.read())
+#print("new text")
+#print(new.read())
+
+print(filecmp.cmp("Example.txt","RevertedFile.txt"))
